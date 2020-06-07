6251b87c99d107613715f122f58637e62777e04e
frefrik <fredrik@haarstad.no>
2020-06-08 01:13:26 +0200

    Add check for status_code 404

--- get_rates.py
+++ get_rates.py
@@ -51,10 +51,10 @@ def nibor():
                 df_panel_new.insert(0, "Date", day)
                 df_panel = df_panel.append(df_panel_new)
             elif load['message'] == 'Invalid request date':
-                print(day, 'No rates published on this date, skipping')
+                #print(day, 'No rates published on this date, skipping')
                 pass
             else:
-                print(day, 'Unknown error, skipping')
+                #print(day, 'Unknown error, skipping')
                 pass
         
         df_panel['Date'] = pd.to_datetime(df_panel['Date'])
@@ -81,18 +81,24 @@ def keyPolicyRate():
     delta = get_dates(date_last)
 
     if delta[2] > 0:
-        df_new = pd.read_csv('https://data.norges-bank.no/api/data/IR/B.KPRA.SD.?format=csv&startPeriod={}&endPeriod={}&locale=en'.format(delta[1], delta[0]),
-                            parse_dates=['TIME_PERIOD'],
-                            delimiter=';',
-                            thousands=',',
-                            usecols=['TIME_PERIOD','OBS_VALUE'])
+        url = 'https://data.norges-bank.no/api/data/IR/B.KPRA.SD.?format=csv&startPeriod={}&endPeriod={}&locale=en'.format(delta[1], delta[0])
+        res = requests.get(url)
 
-        df_new.rename(columns={'TIME_PERIOD':'Date',
-                            'OBS_VALUE':'Rate'}, 
-                         inplace=True)
+        if res.status_code == 404:
+            pass
+        else:
+            df_new = pd.read_csv(url,
+                                parse_dates=['TIME_PERIOD'],
+                                delimiter=';',
+                                thousands=',',
+                                usecols=['TIME_PERIOD','OBS_VALUE'])
 
-        df = df.append(df_new, ignore_index=True)
-        write_df('keyPolicyRate', df)
+            df_new.rename(columns={'TIME_PERIOD':'Date',
+                                'OBS_VALUE':'Rate'}, 
+                            inplace=True)
+
+            df = df.append(df_new, ignore_index=True)
+            write_df('keyPolicyRate', df)
     else:
         print('Data already up to date: keyPolicyRate')
 
@@ -107,27 +113,33 @@ def nowa():
     delta = get_dates(date_last)
 
     if delta[2] > 0:
-        df_new = pd.read_csv('https://data.norges-bank.no/api/data/IR/B.NOWA..?format=csv&startPeriod={}&endPeriod={}&locale=en'.format(delta[1], delta[0]),
-                        parse_dates=['TIME_PERIOD'],
-                        delimiter=';',
-                        thousands=',',
-                        usecols=['TIME_PERIOD',
-                                'Unit of Measure',
-                                'OBS_VALUE',
-                                'Calculation Method'])
+        url = 'https://data.norges-bank.no/api/data/IR/B.NOWA..?format=csv&startPeriod={}&endPeriod={}&locale=en'.format(delta[1], delta[0])
+        res = requests.get(url)
 
-        df_new.rename(columns={'TIME_PERIOD':'Date',
-                                'OBS_VALUE': 'Value',
-                                'Calculation Method': 'Qualifier'},
-                     inplace=True)
-                     
-        qualifier = df_new[['Date', 'Qualifier']].loc[df_new['Unit of Measure'] == 'Rate']
-        df_pivot = pd.pivot_table(df_new, index='Date', columns='Unit of Measure', values='Value')
-        df_new = pd.merge(df_pivot, qualifier, how='right', on=['Date'])
-        df_new = df_new.reindex(columns=['Date','Rate','Volume','Qualifier','Banks lending', 'Banks borrowing', 'Transactions']).replace('Alternative method', 'Alternative').fillna(0)
-        
-        df = df.append(df_new, ignore_index=True)
-        write_df('nowa', df)
+        if res.status_code == 404:
+            pass
+        else:
+            df_new = pd.read_csv(url,
+                            parse_dates=['TIME_PERIOD'],
+                            delimiter=';',
+                            thousands=',',
+                            usecols=['TIME_PERIOD',
+                                    'Unit of Measure',
+                                    'OBS_VALUE',
+                                    'Calculation Method'])
+
+            df_new.rename(columns={'TIME_PERIOD':'Date',
+                                    'OBS_VALUE': 'Value',
+                                    'Calculation Method': 'Qualifier'},
+                        inplace=True)
+                        
+            qualifier = df_new[['Date', 'Qualifier']].loc[df_new['Unit of Measure'] == 'Rate']
+            df_pivot = pd.pivot_table(df_new, index='Date', columns='Unit of Measure', values='Value')
+            df_new = pd.merge(df_pivot, qualifier, how='right', on=['Date'])
+            df_new = df_new.reindex(columns=['Date','Rate','Volume','Qualifier','Banks lending', 'Banks borrowing', 'Transactions']).replace('Alternative method', 'Alternative').fillna(0)
+            
+            df = df.append(df_new, ignore_index=True)
+            write_df('nowa', df)
     else:
         print('Data already up to date: nowa')
 
@@ -142,23 +154,29 @@ def treasuryBills():
     delta = get_dates(date_last)
 
     if delta[2] > 0:
-        df_new = pd.read_csv('https://data.norges-bank.no/api/data/IR/B.TBIL..?format=csv&startPeriod={}&endPeriod={}&locale=en'.format(delta[1], delta[0]),
-                        parse_dates=['TIME_PERIOD'],
-                        delimiter=';',
-                        thousands=',',
-                        usecols=['TIME_PERIOD',
-                                'Tenor',
-                                'OBS_VALUE'])
+        url = 'https://data.norges-bank.no/api/data/IR/B.TBIL..?format=csv&startPeriod={}&endPeriod={}&locale=en'.format(delta[1], delta[0])
+        res = requests.get(url)
 
-        df_new.rename(columns={'TIME_PERIOD':'Date',
-                                'OBS_VALUE': 'Rate'},
-                     inplace=True)
+        if res.status_code == 404:
+            pass
+        else:
+            df_new = pd.read_csv(url,
+                            parse_dates=['TIME_PERIOD'],
+                            delimiter=';',
+                            thousands=',',
+                            usecols=['TIME_PERIOD',
+                                    'Tenor',
+                                    'OBS_VALUE'])
 
-        df_new = pd.pivot_table(df_new, index='Date', columns='Tenor', values='Rate').reset_index()
-        df_new = df_new.reindex(columns=['Date','3 months','6 months','9 months','12 months']).rename_axis(None, axis=1)
+            df_new.rename(columns={'TIME_PERIOD':'Date',
+                                    'OBS_VALUE': 'Rate'},
+                        inplace=True)
 
-        df = df.append(df_new, ignore_index=True)
-        write_df('treasuryBills', df)
+            df_new = pd.pivot_table(df_new, index='Date', columns='Tenor', values='Rate').reset_index()
+            df_new = df_new.reindex(columns=['Date','3 months','6 months','9 months','12 months']).rename_axis(None, axis=1)
+
+            df = df.append(df_new, ignore_index=True)
+            write_df('treasuryBills', df)
     else:
         print('Data already up to date: treasuryBills')
 
@@ -173,24 +191,30 @@ def governmentBonds():
     delta = get_dates(date_last)
 
     if delta[2] > 0:
-        df_new = pd.read_csv('https://data.norges-bank.no/api/data/IR/B.GBON..?format=csv&startPeriod={}&endPeriod={}&locale=en'.format(delta[1], delta[0]),
-                        parse_dates=['TIME_PERIOD'],
-                        delimiter=';',
-                        thousands=',',
-                        usecols=['TIME_PERIOD',
-                                'Tenor',
-                                'OBS_VALUE'])
+        url = 'https://data.norges-bank.no/api/data/IR/B.GBON..?format=csv&startPeriod={}&endPeriod={}&locale=en'.format(delta[1], delta[0])
+        res = requests.get(url)
 
-        df_new.rename(columns={'TIME_PERIOD':'Date',
-                                'OBS_VALUE': 'Rate'},
-                     inplace=True)
+        if res.status_code == 404:
+            pass
+        else:
+            df_new = pd.read_csv(url,
+                            parse_dates=['TIME_PERIOD'],
+                            delimiter=';',
+                            thousands=',',
+                            usecols=['TIME_PERIOD',
+                                    'Tenor',
+                                    'OBS_VALUE'])
 
-        df_new = pd.pivot_table(df_new, index='Date', columns='Tenor', values='Rate').reset_index()
-        df_new = df_new.reindex(columns=['Date','3 years','5 years','10 years']).rename_axis(None, axis=1)
+            df_new.rename(columns={'TIME_PERIOD':'Date',
+                                    'OBS_VALUE': 'Rate'},
+                        inplace=True)
 
-        df = df.append(df_new, ignore_index=True)
+            df_new = pd.pivot_table(df_new, index='Date', columns='Tenor', values='Rate').reset_index()
+            df_new = df_new.reindex(columns=['Date','3 years','5 years','10 years']).rename_axis(None, axis=1)
 
-        write_df('governmentBonds', df)
+            df = df.append(df_new, ignore_index=True)
+
+            write_df('governmentBonds', df)
     else:
         print('Data already up to date: governmentBonds')
 
@@ -205,26 +229,32 @@ def exchangeRates():
     delta = get_dates(date_last)
 
     if delta[2] > 0:
-        df_new = pd.read_csv('https://data.norges-bank.no/api/data/EXR/B..NOK.SP?format=csv&startPeriod={}&endPeriod={}&locale=en'.format(delta[1], delta[0]),
-                        parse_dates=['TIME_PERIOD'],
-                        delimiter=';',
-                        thousands=',',
-                        usecols=['TIME_PERIOD',
-                                'BASE_CUR',
-                                'QUOTE_CUR',
-                                'OBS_VALUE'])
+        url = 'https://data.norges-bank.no/api/data/EXR/B..NOK.SP?format=csv&startPeriod={}&endPeriod={}&locale=en'.format(delta[1], delta[0])
+        res = requests.get(url)
 
-        df_new.rename(columns={'TIME_PERIOD': 'Date',
-                                'BASE_CUR': 'Base Currency',
-                                'QUOTE_CUR': 'Quote Currency',
-                                'OBS_VALUE': 'Rate'},
-                     inplace=True)
+        if res.status_code == 404:
+            pass
+        else:
+            df_new = pd.read_csv(url,
+                            parse_dates=['TIME_PERIOD'],
+                            delimiter=';',
+                            thousands=',',
+                            usecols=['TIME_PERIOD',
+                                    'BASE_CUR',
+                                    'QUOTE_CUR',
+                                    'OBS_VALUE'])
 
-        df_new = pd.pivot_table(df_new, index=['Date', 'Quote Currency'], columns='Base Currency', values='Rate').reset_index().rename_axis(None, axis=1)
+            df_new.rename(columns={'TIME_PERIOD': 'Date',
+                                    'BASE_CUR': 'Base Currency',
+                                    'QUOTE_CUR': 'Quote Currency',
+                                    'OBS_VALUE': 'Rate'},
+                        inplace=True)
 
-        df = df.append(df_new, ignore_index=True)
+            df_new = pd.pivot_table(df_new, index=['Date', 'Quote Currency'], columns='Base Currency', values='Rate').reset_index().rename_axis(None, axis=1)
 
-        write_df('exchangeRates', df)
+            df = df.append(df_new, ignore_index=True)
+
+            write_df('exchangeRates', df)
     else:
         print('Data already up to date: exchangeRates')
 
